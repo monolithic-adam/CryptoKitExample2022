0be5050fe1307bd866fbd07ee4f5c3271c13e3bd
monolithic-adam <adamjhen@gmail.com>
2022-08-16 16:49:24 +0900

    Initial Commit

---
+++ CryptoKitExample2022/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CryptoKitExample2022
+//
+//  Created by Adam Henry on 2022/08/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CryptoKitExample2022/CryptoKitExample2022App.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoKitExample2022App.swift
+//  CryptoKitExample2022
+//
+//  Created by Adam Henry on 2022/08/16.
+//
+
+import SwiftUI
+
+@main
+struct CryptoKitExample2022App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
